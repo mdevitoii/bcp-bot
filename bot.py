7bc61f6ca82e7b999a5f2f64c331992df6f4ca43
Michael DeVito <mdevitoii04@gmail.com>
2026-02-05 21:36:29 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,6 +35,7 @@ intents.message_content = True
 async def get_prefix(bot, message):
 
     p = db.getPrefix(message.guild.id)
+    print(p)
     
     return commands.when_mentioned_or(p)(bot, message)
 
