1959300807b600a24dbfd47f4a7e30f8d8d0c5a8
mdevitoii <mdevitoii04@gmail.com>
2026-02-10 11:04:01 -0500

    Update database.py

--- database.py
+++ database.py
@@ -195,6 +195,7 @@ def setStatus(server_id, status):
 def getTodaysCollect():
     print("GETTING TODAY COLLECT")
     today = datetime.today().strftime('%m-%d') # gets today in MM-DD format
+    print(today)
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
     c.execute("SELECT collect FROM collects WHERE date = ?", (today,))
